2f4c4fe3219f222672ea545f54904d3926b27afb
Ashwin Ajit <aswin.akrp@example.com>
2026-02-02 13:29:44 +0530

    Day 3: loops

---
+++ day3_loops.py
@@ -0,0 +1,19 @@
+#for i in range(0,11):
+ #   print(i)
+
+
+#name = "Ashwin"
+#for char in name:
+ #   print(char)
+
+#while True:
+ #   number = int(input("Enter a number, 0 to stop : "))
+
+  #  if number == 0:
+   #     break
+
+#print("You entered: ", number)
+
+#for i in range(1, 21):
+ #   if i % 2 != 0:
+  #      print(i)
