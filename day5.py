07899c71713630d72a90f606d404dfa15ad6ca49
Ashwin Ajit <aswin.akrp@example.com>
2026-03-03 16:54:51 +0530

    Day 5: Dictionaries

---
+++ day5.py
@@ -0,0 +1,16 @@
+student = {
+    "name": "Ashwin",
+    "age": 56,
+    "work": "none"
+
+}
+
+student["name"] = "Siyal"
+student["city"] = "TN"
+
+for key, value in student.items():
+    print(key, value)
+
+print(student["city"])
+
+
