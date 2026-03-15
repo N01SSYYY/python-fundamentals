61ccfce29b3f9497b4f883b602392d68410b2146
Ashwin Ajit <aswin.akrp@example.com>
2026-03-15 20:16:01 +0530

    Day 6: Functions

---
+++ day6.py
@@ -0,0 +1,35 @@
+def welcome(name):
+    print("Welcome", name)
+
+welcome("Ashwin")
+
+def greet(name):
+    print("Welcome", name)
+
+greet("Adarsh")
+
+def add(a, b):
+    return a + b
+
+result = add(7, 8)
+print(result)
+
+def square(n):
+    return n * n
+
+result = 5 * 5
+
+def oddoreven(n):
+    if (n % 2 == 1):
+        print("This number is not even")
+        
+    else: 
+        print("The number is even")
+
+n = int(input("Enter a number: "))
+oddoreven(n)
+
+
+    
+
+    
